5cbe9681ba4c8c339637beed9004bef8842e3be5
louiscklaw <louiscklaw@gmail.com>
2020-04-10 04:27:00 +0800

    init helloworld,

--- .travis/merge.py
+++ .travis/merge.py
@@ -153,7 +153,7 @@ def merge_to_develop_branch(branch_to_merge, cwd):
 def merge_to_pre_merge_master_branch(branch_to_merge, cwd):
   create_branch_if_not_exist('testingbranch', cwd)
   print('findme1')
-  run_command("git push", cwd)
+  # run_command("git push", cwd)
   # push_commit(PUSH_URI, 'testingbranch', cwd)
   # run_command('git merge --ff-only "{}"'.format(branch_to_merge), cwd)
 
