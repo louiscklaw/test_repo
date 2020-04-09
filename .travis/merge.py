0c2602bacffde71f43b20f9f027221650f45d604
louiscklaw <louiscklaw@gmail.com>
2020-04-10 03:09:33 +0800

    ci: test with adding env,

--- .travis/merge.py
+++ .travis/merge.py
@@ -152,7 +152,8 @@ def merge_to_develop_branch(branch_to_merge, cwd):
 
 def merge_to_pre_merge_master_branch(branch_to_merge, cwd):
   create_branch_if_not_exist('pre-merge-master', cwd)
-  run_command('git merge --ff-only "{}"'.format(branch_to_merge), cwd)
+  print('findme1')
+  # run_command('git merge --ff-only "{}"'.format(branch_to_merge), cwd)
 
 def merge_to_master_branch(branch_to_merge, cwd):
   checkout_branch('master', cwd)
